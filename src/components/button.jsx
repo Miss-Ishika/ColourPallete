28d3a20396b5d3fdd451cff952248eb1b8e6fac1
Miss-Ishika <101314286+Miss-Ishika@users.noreply.github.com>
2024-04-25 17:07:07 +0530

    first commit

---
+++ src/components/button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function But({colour}){
+
+    return (
+        <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: colour}}>Red</button>
+
+    )
+
+}export default But
